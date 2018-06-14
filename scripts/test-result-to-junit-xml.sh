590f2a04385761284b48e6dd24a66657ee851b8e
Teerayut Hiruntaraporn <teerayut.h@throughwave.co.th>
2018-06-14 15:10:09 +0700

    Verbose current working directory

--- scripts/test-result-to-junit-xml.sh
+++ scripts/test-result-to-junit-xml.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "PATH: $(dirname $0)"
-echo "===================="
 cat $(dirname $0)/../$1.json | $(dirname $0)/../node_modules/.bin/cucumber-junit > ./test-result/$1.xml
-echo "=========="
-ls -l $(dirname $0)
+echo "$PWD: ==============="
+ls -lr 
