d93b5972cd5b9d030289b07b305b407867764463
Teerayut Hiruntaraporn <teerayut.h@throughwave.co.th>
2018-06-14 15:02:08 +0700

    Verbose output when convert test result

--- scripts/test-result-to-junit-xml.sh
+++ scripts/test-result-to-junit-xml.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "PATH: $(dirname $0)"
+echo "===================="
 cat $(dirname $0)/../$1.json | $(dirname $0)/../node_modules/.bin/cucumber-junit > ./test-result/$1.xml
+echo "=========="
+ls -l $(dirname $0)
